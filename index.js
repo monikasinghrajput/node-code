039edb3222af09c8cb9c93894018385ec1866c21
monikasinghrajput <ms6139087@gmail.com>
2024-08-22 13:26:57 +0530

    test3

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ exports.handler = async (event) => {
     // Your existing code
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: 'This site is running good' }),
+        body: JSON.stringify({ message: 'This site is running' }),
     };
 };
 
